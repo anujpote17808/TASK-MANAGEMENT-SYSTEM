f362ff7853f48565989438dce3878d0009773233
Anuj Sanjay Pote <anujpote17808@gmail.com>
2026-09-13 11:13:50 +0530

    Configure backend for Render port

--- task-management-backend/src/server.js
+++ task-management-backend/src/server.js
@@ -1,6 +1,4 @@
-// Load environment variables FIRST
 require("dotenv").config({ override: true });
-
 
 const express = require("express");
 const cors = require("cors");
@@ -10,45 +8,30 @@ const connectDB = require("./config/db");
 const taskRoutes = require("./routes/taskRoutes");
 const authRoutes = require("./routes/authRoutes");
 
-
 const app = express();
 
-const PORT = 5001;
-
-
-// Connect MongoDB
 connectDB();
 
-
-// Middleware
 app.use(cors());
-
 app.use(express.json());
 
-
-// Home Route
 app.get("/", (req, res) => {
-
     res.json({
         message: "Task Management Backend is running"
     });
-
 });
-
 
 // Authentication Routes
 app.use("/api/auth", authRoutes);
 
-
 // Task Routes
 app.use("/api/tasks", taskRoutes);
 
-
 // Start Server
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 5001;
 
+app.listen(PORT, "0.0.0.0", () => {
     console.log(
-        `Server running on http://localhost:${PORT}`
+        `Server running on port ${PORT}`
     );
-
 });
